8bb502f12f543d058af1a94d0221ab6be21017a9
bestcb2333 <2892709432@qq.com>
2025-07-24 14:41:33 +0800

    fix: 修改包名

--- scoper.go
+++ scoper.go
@@ -1,92 +1,88 @@
-package main
+package scoper
 
 import (
 	"gorm.io/gorm"
 )
 
-// ScopeOption 定义scope选项的函数类型
-type ScopeOption func(*gorm.DB) *gorm.DB
-
-// ScopeBuilder 创建一个组合多个选项的scope函数
-func NewScope(options ...ScopeOption) func(*gorm.DB) *gorm.DB {
+// Select 设置查询字段
+func Model(value any) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
-		for _, option := range options {
-			db = option(db)
-		}
-		return db
+		return db.Model(value)
 	}
 }
 
 // Select 设置查询字段
-func Select(fields ...string) ScopeOption {
+func Select(fields ...string) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		return db.Select(fields)
 	}
 }
 
 // Where 添加where条件
-func Where(query interface{}, args ...interface{}) ScopeOption {
+func Where(query any, args ...any) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		return db.Where(query, args...)
 	}
 }
 
 // Limit 设置查询限制数量
-func Limit(limit int) ScopeOption {
+func Limit(limit int) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		return db.Limit(limit)
 	}
 }
 
 // Offset 设置查询偏移量
-func Offset(offset int) ScopeOption {
+func Offset(offset int) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		return db.Offset(offset)
 	}
 }
 
 // Order 设置排序
-func Order(order string) ScopeOption {
+func Order(order string) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		return db.Order(order)
 	}
 }
 
 // Group 设置分组
-func Group(group string) ScopeOption {
+func Group(group string) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		return db.Group(group)
 	}
 }
 
 // Having 设置having条件
-func Having(query interface{}, args ...interface{}) ScopeOption {
+func Having(query any, args ...any) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		return db.Having(query, args...)
 	}
 }
 
 // Joins 设置join连接
-func Joins(query string, args ...interface{}) ScopeOption {
+func Joins(query string, args ...any) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		return db.Joins(query, args...)
 	}
 }
 
 // Preload 设置预加载关联，支持嵌套选项
-func Preload(association string, options ...ScopeOption) ScopeOption {
+func Preload(association string, options ...any) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
-		if len(options) > 0 {
-			// 如果有嵌套选项，创建一个组合的scope函数
-			nestedScope := NewScope(options...)
-			return db.Preload(association, nestedScope)
-		}
-		return db.Preload(association)
+		return db.Preload(association, options...)
+	}
+}
+
+// 分页
+func Page(page, pageSize int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Offset((page - 1) * pageSize).Limit(pageSize)
 	}
 }
 
 // Distinct 设置去重
-func Distinct(columns ...string) ScopeOption {
+func Distinct(columns ...string) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		if len(columns) > 0 {
 			return db.Distinct(columns)
@@ -96,14 +92,14 @@ func Distinct(columns ...string) ScopeOption {
 }
 
 // Omit 忽略指定字段
-func Omit(columns ...string) ScopeOption {
+func Omit(columns ...string) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		return db.Omit(columns...)
 	}
 }
 
 // Scopes 应用其他自定义scope
-func Scopes(scopes ...func(*gorm.DB) *gorm.DB) ScopeOption {
+func Scopes(scopes ...func(*gorm.DB) *gorm.DB) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
 		return db.Scopes(scopes...)
 	}
